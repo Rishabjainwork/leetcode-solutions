c12d0ebe6f0017df368fec874b9ad857367991c5
Rishab jain <rishabjain.workpro@gmail.com>
2026-09-03 15:04:38 +0530

    Time: 0 ms (100%), Space: 165.7 MB (99.48%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -16,3 +16,9 @@ public:
         return true;
     }
 };
+// The idea is to make all numbers odd.
+
+//  Find the smallest odd number.
+//  Any even number smaller than this cannot subtract an odd number because there is no smaller valid element.
+//  So if even < smallestOdd, return false.
+//  Otherwise, return true.
